bfd995ddea305fddedea166c0ada9b46337cb2ae
karolb11 <karolbroniec@gmail.com>
2020-12-05 17:03:04 +0100

    integraton with angular, no socket working

--- src/main/java/com/example/websockets/config/WebSocketConfig.java
+++ src/main/java/com/example/websockets/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket.io");
+        registry.addEndpoint("/messages");
     }
 
 }
